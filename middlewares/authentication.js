272cfccf205c46cb611880bafd47e9460c4449f0
Hernan Pieroni <hernan.pieroni@gmail.com>
2020-02-02 16:26:18 -0300

    feat: create authentication middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,32 @@
+const { Unauthorized } = require('http-errors');
+
+/**
+ * Validates the presence of a valid token in Authorization header
+ *
+ * @param {*} req req object
+ * @param {*} res res object
+ * @param {*} next next function
+ *
+ * @throws { Unauthorized } 401 HTTP Unauthorized error
+ */
+const authentication = validToken => (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new Unauthorized('Missing authorization header');
+  }
+
+  if (!authHeader.startsWith('Bearer ')) {
+    throw new Unauthorized('Invalid authorization schema');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  if (token !== validToken) {
+    throw new Unauthorized('Invalid token');
+  }
+
+  next();
+};
+
+module.exports = authentication;
---
+++ middlewares/authentication.test.js
@@ -0,0 +1,39 @@
+const { Unauthorized } = require('http-errors');
+const authentication = require('./authentication');
+
+describe('Authentication middleware', () => {
+  let auth;
+  let req;
+  let next;
+
+  beforeEach(() => {
+    auth = authentication('validToken');
+    req = {
+      headers: {}
+    };
+    next = jest.fn();
+  });
+
+  test('should throw an error if no authorization header is provided', () => {
+    expect(() => auth(req, {}, next)).toThrow(new Unauthorized('Missing authorization header'));
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test('should throw an error if no authorization schema is invalid', () => {
+    req.headers.authorization = 'InvalidShema 123123';
+    expect(() => auth(req, {}, next)).toThrow(new Unauthorized('Invalid authorization schema'));
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test('should throw an error if token is invalid', () => {
+    req.headers.authorization = 'Bearer invalidToken';
+    expect(() => auth(req, {}, next)).toThrow(new Unauthorized('Invalid token'));
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test('should call next middleware if auth is successfull ', () => {
+    req.headers.authorization = 'Bearer validToken';
+    auth(req, {}, next);
+    expect(next).toHaveBeenCalled();
+  });
+});
